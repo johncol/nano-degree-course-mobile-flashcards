687673dbd11b47c9cd9906af1e83df4d6fe99b59
John <john.19col@gmail.com>
2019-03-02 18:51:05 -0500

    Add combined reducer

---
+++ state/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import decks from './decks';
+import cards from './cards';
+
+const reducers = combineReducers({
+  decks,
+  cards
+});
+
+export default reducers;
