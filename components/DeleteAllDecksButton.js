ae56a8b2087db338e32a88d182b35d0dc5485666
John <john.19col@gmail.com>
2019-03-09 13:51:24 -0500

    Create DeleteAllDecksButton component

---
+++ components/DeleteAllDecksButton.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+
+import { DeckAction } from '../state/actions/deck';
+
+class DeleteAllDecksButton extends Component {
+  render() {
+    const { hasZeroDecks, deleteAllDecks } = this.props;
+    if (hasZeroDecks) {
+      return null;
+    }
+
+    return (
+      <View style={styles.container}>
+        <Button title="Delete all decks" onPress={deleteAllDecks} type="outline" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    marginHorizontal: 20
+  }
+});
+
+const stateToProps = state => ({
+  hasZeroDecks: Object.keys(state.decks).length === 0
+});
+
+const dispatchToProps = dispatch => ({
+  deleteAllDecks: () => {
+    dispatch(DeckAction.deleteAllDecks());
+  }
+});
+
+export default connect(
+  stateToProps,
+  dispatchToProps
+)(DeleteAllDecksButton);
