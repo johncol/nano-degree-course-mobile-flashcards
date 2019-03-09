ae1924d12f80932474795cd1f92c4f145e29047b
John <john.19col@gmail.com>
2019-03-09 17:51:16 -0500

    Create QuizQuestion component

---
+++ components/QuizQuestion.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { withNavigation } from 'react-navigation';
+
+import ButtonsContainer from './ButtonsContainer';
+import QuizButtonsContainer from './QuizButtonsContainer';
+import QuizButton from './QuizButton';
+
+class QuizQuestion extends Component {
+  state = { answerVisible: false };
+
+  showAnswer = () => this.setState({ answerVisible: true });
+  hideAnswer = () => this.setState({ answerVisible: false });
+
+  next = () => {
+    this.props.nextCard();
+    this.hideAnswer();
+  };
+
+  incrementAndNext = () => {
+    this.props.incrementScore();
+    this.next();
+  };
+
+  render() {
+    const { answerVisible } = this.state;
+    const { card } = this.props;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.question}>{card.question}</Text>
+
+        {!answerVisible && (
+          <ButtonsContainer>
+            <Button title="Show answer" onPress={this.showAnswer} />
+          </ButtonsContainer>
+        )}
+
+        {answerVisible && (
+          <React.Fragment>
+            <Text style={styles.answer}>{card.answer}</Text>
+            <QuizButtonsContainer>
+              <QuizButton title="Correct" onPress={this.incrementAndNext} />
+              <QuizButton title="Incorrect" onPress={this.next} />
+            </QuizButtonsContainer>
+          </React.Fragment>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  question: {
+    marginTop: 20,
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  answer: {
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 30,
+    textAlign: 'center'
+  }
+});
+
+const stateToProps = (state, props) => {
+  const cardsIds = props.deck.cards;
+  const cards = cardsIds.map(cardId => state.cards[cardId]);
+  const card = cards[props.cardIndex];
+
+  return {
+    ...props,
+    card
+  };
+};
+
+const dispatchToProps = dispatch => ({});
+
+export default withNavigation(
+  connect(
+    stateToProps,
+    dispatchToProps
+  )(QuizQuestion)
+);
