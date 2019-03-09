0aa2eaaae540f06d5652a2bd50daa21051a8e085
John <john.19col@gmail.com>
2019-03-09 17:51:24 -0500

    Create QuizResult component

---
+++ components/QuizResult.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { Button } from 'react-native-elements';
+
+import ButtonsContainer from './ButtonsContainer';
+import QuizScore from './QuizScore';
+import Navigator from '../navigation/navigator';
+
+class QuizResult extends React.Component {
+  goToDeck = () => {
+    Navigator.goBack(this);
+  };
+
+  render() {
+    const { score, cardsCount, onRestart: restartQuiz } = this.props;
+    return (
+      <View style={styles.container}>
+        <QuizScore score={score} total={cardsCount} />
+
+        <ButtonsContainer height={100}>
+          <Button title="Try again" onPress={restartQuiz} />
+          <Button title="Go back to deck" onPress={this.goToDeck} />
+        </ButtonsContainer>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default withNavigation(QuizResult);
