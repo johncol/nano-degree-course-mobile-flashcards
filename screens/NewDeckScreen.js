9c749e92dd1d4cce7cc84097f267dbf921a026c5
John <john.19col@gmail.com>
2019-03-09 07:50:38 -0500

    Create NewDeckScreen component

---
+++ screens/NewDeckScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import NewDeckForm from '../components/NewDeckForm';
+import ScreenTitle from '../components/ScreenTitle';
+import ScreenInfo from '../components/ScreenInfo';
+import ScreenContainer from '../components/ScreenContainer';
+import ScreenContent from '../components/ScreenContent';
+
+export default class NewDeckScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return (
+      <ScreenContainer>
+        <ScreenContent>
+          <ScreenTitle>Add a new Deck</ScreenTitle>
+          <NewDeckForm />
+        </ScreenContent>
+        <ScreenInfo>Decks allow you to group cards and take quizzes</ScreenInfo>
+      </ScreenContainer>
+    );
+  }
+}
