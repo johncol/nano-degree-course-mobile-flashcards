19609e478423fd309d27b5cc91d809c338295d8c
John <john.19col@gmail.com>
2019-03-09 07:52:28 -0500

    Add NewDeckStack and DecksStack to the MainTabNavigator component

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,57 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import DecksScreen from '../screens/DecksScreen';
+import NewDeckScreen from '../screens/NewDeckScreen';
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: HomeScreen
 });
-
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+    <TabBarIcon focused={focused} name="md-information-circle" />
+  )
 };
 
 const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+  Links: LinksScreen
 });
-
 LinksStack.navigationOptions = {
   tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-link" />
 };
 
 const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+  Settings: SettingsScreen
 });
-
 SettingsStack.navigationOptions = {
   tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-options" />
+};
+
+const DecksStack = createStackNavigator({
+  Decks: DecksScreen
+});
+DecksStack.navigationOptions = {
+  tabBarLabel: 'Decks',
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />
+};
+
+const NewDeckStack = createStackNavigator({
+  Decks: NewDeckScreen
+});
+NewDeckStack.navigationOptions = {
+  tabBarLabel: 'New Deck',
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-add" />
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+  NewDeckStack,
+  DecksStack,
+  LinksStack
 });
