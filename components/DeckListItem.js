7ea6be04f0ddf10399f8a30ec68d7c3aa1a5a688
John <john.19col@gmail.com>
2019-03-09 15:43:27 -0500

    Update DeckListItem to show cards count and navigate to the deck screen

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { ListItem } from 'react-native-elements';
+import { withNavigation } from 'react-navigation';
+
+import Navigator from '../navigation/navigator';
 
 class DeckListItem extends Component {
+  navigateToDeckScreen = () => {
+    const { deck } = this.props;
+    Navigator.navigateToDeck(this, deck);
+  };
+
   render() {
     const { deck } = this.props;
+    const cardsCount = deck.cards.length;
     return (
       <ListItem
         title={deck.name}
         subtitle={'Created on ' + deck.creationDate}
-        rightTitle={deck.cards.length + ' cards'}
+        rightTitle={cardsCount + ' card' + (cardsCount !== 1 ? 's' : '')}
         chevron={true}
+        onPress={this.navigateToDeckScreen}
       />
     );
   }
 }
 
-export default DeckListItem;
+export default withNavigation(DeckListItem);
