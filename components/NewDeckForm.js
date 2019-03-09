dd46c28a77bd95fe5564dfa804cbb91eedb9e810
John <john.19col@gmail.com>
2019-03-09 11:06:27 -0500

    Complete NewDeckForm implementation with logic to create a new Deck

--- components/NewDeckForm.js
+++ components/NewDeckForm.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Input } from 'react-native-elements';
+import { connect } from 'react-redux';
+
+import { DeckAction } from './../state/actions/deck';
 
 class NewDeckForm extends Component {
+  state = {
+    deckName: ''
+  };
+
+  updateDeckName = deckName => {
+    this.setState({ deckName });
+  };
+
+  createDeck = () => {
+    const { deckName } = this.state;
+    const { createDeck } = this.props;
+    createDeck(deckName);
+    this.setState({ deckName: '' });
+  };
+
   render() {
+    const { deckName } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.input}>
-          <Input label="Deck name" placeholder="e.g. Game of Thrones trivia" />
+          <Input
+            label="Deck name"
+            placeholder="e.g. Game of Thrones trivia"
+            onChangeText={this.updateDeckName}
+            value={deckName}
+          />
         </View>
-        <Button title="Create" />
+        <Button
+          title="Create"
+          onPress={this.createDeck}
+          disabled={deckName.trim() === ''}
+        />
       </View>
     );
   }
@@ -29,4 +57,13 @@ const styles = StyleSheet.create({
   }
 });
 
-export default NewDeckForm;
+const dispatchToProps = dispatch => ({
+  createDeck: name => {
+    dispatch(DeckAction.createDeck(name));
+  }
+});
+
+export default connect(
+  undefined,
+  dispatchToProps
+)(NewDeckForm);
